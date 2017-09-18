797068074598375e7e32849cfc8339c1eddf20d9
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-09-18 18:33:58 +0200

    Added dotenv

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+import dotenv from 'dotenv'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import { App } from './App'
 import registerServiceWorker from './registerServiceWorker'
+
+dotenv.config()
 
 window.React = React
 ReactDOM.render(<App />, document.getElementById('root'))
