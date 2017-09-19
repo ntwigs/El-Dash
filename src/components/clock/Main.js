9b5e7d93eac03b4053dc160e90bed2fde13d2cb2
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-09-19 23:23:46 +0200

    Main displays commits and clock with digital

--- src/components/clock/Main.js
+++ src/components/clock/Main.js
@@ -4,16 +4,11 @@ import { compose, withState, lifecycle } from 'recompose'
 import { Number } from './Number'
 import * as display from '../../utils/numbers'
 
-const NumberContainer = styled.div`
-  display: flex;
-`
+const NumberContainer = styled.div`display: flex;`
 
-const addZero = value => `0${value}`
+const addZero = value => `0${ value }`
 const extractValue = value => value.slice(-2)
-const correctNumbers = compose(
-  extractValue,
-  addZero,
-)
+const correctNumbers = compose(extractValue, addZero)
 
 const getHours = ({ index }) => {
   const date = new Date().getHours().toString()
@@ -22,9 +17,26 @@ const getHours = ({ index }) => {
 }
 
 const getMinutes = ({ index }) => {
-  const date = new Date().getSeconds().toString()
+  const date = new Date().getMinutes().toString()
   const getCorrectNumbers = correctNumbers(date)[index]
   return display.getNumberAsText({ number: getCorrectNumbers })
+}
+
+const getCommits = async () => {
+  const userData = await fetch('https://api.github.com/users/NorthernTwig/events', {
+    headers: {
+      Authorization: `token ${ process.env.REACT_APP_GITHUB_API_KEY }`,
+      'User-Agent': 'NorthernTwig',
+    },
+  })
+  const userDataJson = await userData.json()
+  const today = new Date().getDate()
+  return userDataJson
+    .filter(
+      ({ created_at, payload }) => today === new Date(created_at).getDate() && 'commits' in payload,
+    )
+    .map(({ payload }) => payload.commits.length)
+    .reduce((acc, val) => acc + val, 0)
 }
 
 const enhance = compose(
@@ -32,8 +44,16 @@ const enhance = compose(
   withState('secondHour', 'setSecondHour', () => getHours({ index: 1 })),
   withState('firstMinute', 'setFirstMinute', () => getMinutes({ index: 0 })),
   withState('secondMinute', 'setSecondMinute', () => getMinutes({ index: 1 })),
+  withState('commits', 'setCommits', 0),
+  withState('displayCommits', 'setDisplayCommits', false),
   lifecycle({
-    componentDidMount() {
+    async componentDidMount() {
+      this.props.setCommits(await getCommits())
+      setInterval(async () => {
+        this.props.setCommits(await getCommits())
+        this.props.setDisplayCommits(!this.props.displayCommits)
+      }, 5000)
+
       setInterval(() => {
         this.props.setFirstHour(getHours({ index: 0 }))
         this.props.setSecondHour(getHours({ index: 1 }))
@@ -44,14 +64,21 @@ const enhance = compose(
   }),
 )
 
+const getValue = ({ index, commits }) => {
+  const reversedCommits = commits.toString().split('').reverse().join('')
+  const number = reversedCommits.toString()[index] || '0'
+  const numberAsText = display.getNumberAsText({ number })
+  return display[numberAsText]
+}
+
 export const Clock = enhance(
-  ({ firstHour, secondHour, firstMinute, secondMinute }) => (
+  ({ firstHour, secondHour, firstMinute, secondMinute, displayCommits, commits }) => (
     <NumberContainer>
-      <Number display={display[firstHour]} />
-      <Number display={display[secondHour]} />
-      <Number display={display.colon} />
-      <Number display={display[firstMinute]} />
-      <Number display={display[secondMinute]} />
+      <Number display={ displayCommits ? getValue({ index: 4, commits }) : display[firstHour] } />
+      <Number display={ displayCommits ? getValue({ index: 3, commits }) : display[secondHour] } />
+      <Number display={ displayCommits ? getValue({ index: 2, commits }) : display.colon } />
+      <Number display={ displayCommits ? getValue({ index: 1, commits }) : display[firstMinute] } />
+      <Number display={ displayCommits ? getValue({ index: 0, commits }) : display[secondMinute] } />
     </NumberContainer>
   ),
 )
