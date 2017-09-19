6691ffae293ad5e86c7e82ec16bab826ca90369c
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-09-19 23:23:24 +0200

    Removed small field from index

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,13 +1,12 @@
 import * as React from 'react'
 import styled from 'styled-components'
 import { BigContainer } from './BigContainer'
-import { SmallContainer } from './SmallContainer'
 import { background } from '../utils/colors'
 
 const PageContainer = styled.section`
   width: 100vw;
   height: 100vh;
-  background-color: ${background};
+  background-color: ${ background };
   display: flex;
   justify-content: center;
   align-items: center;
@@ -18,6 +17,5 @@ const PageContainer = styled.section`
 export const Index = () => (
   <PageContainer>
     <BigContainer />
-    <SmallContainer />
   </PageContainer>
 )
