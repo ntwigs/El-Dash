48283b2e700e64a1f59ecd36a209ffb2d3b8e555
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-09-20 20:19:45 +0200

    Main text displays commits

--- src/components/text/Main.js
+++ src/components/text/Main.js
@@ -4,17 +4,22 @@ import { Number } from './Number'
 import * as display from '../../utils/letters'
 
 const Container = styled.div`
-  background-color: red;
   display: flex;
   justify-content: center;
   align-items: center;
+  transform: scale(0.5);
 `
 
-export const Text = () =>
+const getText = ({ displayCommits, displayTime }) => (
   <Container>
-    <Number display={ display.a } />
-    <Number display={ display.a } />
-    <Number display={ display.a } />
-    <Number display={ display.a } />
-    <Number display={ display.a } />
+    <Number display={ displayCommits ? display.c : display.a } />
+    <Number display={ displayCommits ? display.o : display.a } />
+    <Number display={ displayCommits ? display.m : display.a } />
+    <Number display={ displayCommits ? display.m : display.a } />
+    <Number display={ displayCommits ? display.i : display.a } />
+    <Number display={ displayCommits ? display.t : display.a } />
+    <Number display={ displayCommits ? display.s : display.a } />
   </Container>
+)
+
+export const Text = props => <Container>{getText(props)}</Container>
