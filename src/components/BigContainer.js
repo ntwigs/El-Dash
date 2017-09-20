331d7c2d879f31c62a896536515b989c11e40aa0
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-09-20 19:31:48 +0200

    Added text to big container

--- src/components/BigContainer.js
+++ src/components/BigContainer.js
@@ -1,4 +1,10 @@
 import * as React from 'react'
 import { Clock } from './clock/Main'
+import { Text } from './text/Main'
 
-export const BigContainer = () => <Clock />
+export const BigContainer = () => (
+  <div>
+    <Text />
+    <Clock />
+  </div>
+)
