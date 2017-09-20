6d0daa556065490b7e6a5c3c23bd08fb00ba85ed
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-09-20 20:19:24 +0200

    Added letters for commit

--- src/utils/letters.js
+++ src/utils/letters.js
@@ -1,4 +1,4 @@
-const a = [
+export const a = [
   true, true, true,
   true, false, true,
   true, false, true,
@@ -6,4 +6,64 @@ const a = [
   true, false, true,
   true, false, true,
   true, false, true,
+]
+
+export const c = [
+  true, true, true,
+  true, false, false,
+  true, false, false,
+  true, false, false,
+  true, false, false,
+  true, false, false,
+  true, true, true,
+]
+
+export const o = [
+  true, true, true,
+  true, false, true,
+  true, false, true,
+  true, false, true,
+  true, false, true,
+  true, false, true,
+  true, true, true,
+]
+
+export const m = [
+  true, false, true,
+  true, true, true,
+  true, false, true,
+  true, false, true,
+  true, false, true,
+  true, false, true,
+  true, false, true,
+]
+
+export const i = [
+  false, true, false,
+  false, true, false,
+  false, true, false,
+  false, true, false,
+  false, true, false,
+  false, true, false,
+  false, true, false,
+]
+
+export const t = [
+  true, true, true,
+  false, true, false,
+  false, true, false,
+  false, true, false,
+  false, true, false,
+  false, true, false,
+  false, true, false,
+]
+
+export const s = [
+  false, true, false,
+  true, false, true,
+  true, false, false,
+  false, true, false,
+  false, false, true,
+  true, false, true,
+  false, true, false,
 ]
