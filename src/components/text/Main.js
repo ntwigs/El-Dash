3818aab08004da9bfaef079ecab9dda81dc417c9
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-09-20 19:38:25 +0200

    Can display letters above clock

--- src/components/text/Main.js
+++ src/components/text/Main.js
@@ -1,4 +1,20 @@
 import * as React from 'react'
+import styled from 'styled-components'
+import { Number } from './Number'
+import * as display from '../../utils/letters'
 
+const Container = styled.div`
+  background-color: red;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
-export const Text = () => <h1>HEJSAN</h1>
+export const Text = () =>
+  <Container>
+    <Number display={ display.a } />
+    <Number display={ display.a } />
+    <Number display={ display.a } />
+    <Number display={ display.a } />
+    <Number display={ display.a } />
+  </Container>
