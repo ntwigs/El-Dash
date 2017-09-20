eac9041721a9da1aa8b0824007881796c4278f27
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-09-20 20:19:14 +0200

    Big container chooses what to display

--- src/components/BigContainer.js
+++ src/components/BigContainer.js
@@ -1,10 +1,25 @@
 import * as React from 'react'
+import { compose, withState, lifecycle } from 'recompose'
 import { Clock } from './clock/Main'
 import { Text } from './text/Main'
 
-export const BigContainer = () => (
+const enhance = compose(
+  withState('displayTime', 'setDisplayTime', true),
+  withState('displayCommits', 'setDisplayCommits', false),
+  lifecycle({
+    componentDidMount() {
+      const { setDisplayTime, setDisplayCommits } = this.props
+      setInterval(() => {
+        setDisplayTime(!this.props.displayTime)
+        setDisplayCommits(!this.props.displayCommits)
+      }, 5000)
+    },
+  }),
+)
+
+export const BigContainer = enhance(props => (
   <div>
-    <Text />
+    <Text { ...props } />
     <Clock />
   </div>
-)
+))
