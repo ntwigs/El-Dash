5736e17ad0f3a8a4af9bc05a76081cec586895b1
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-09-23 23:38:54 +0200

    Removed annoying switch

--- src/components/BigContainer.js
+++ src/components/BigContainer.js
@@ -4,27 +4,13 @@ import { compose, withState, lifecycle } from 'recompose'
 import { Clock } from './information/Main'
 import { Text } from './text/Main'
 
-const enhance = compose(
-  withState('displayTime', 'setDisplayTime', true),
-  withState('displayCommits', 'setDisplayCommits', false),
-  lifecycle({
-    componentDidMount() {
-      const { setDisplayTime, setDisplayCommits } = this.props
-      setInterval(() => {
-        setDisplayTime(!this.props.displayTime)
-        setDisplayCommits(!this.props.displayCommits)
-      }, 5000)
-    },
-  }),
-)
-
 const Container = styled.div`
   width: 600px;
 `
 
-export const BigContainer = enhance(props => (
+export const BigContainer = props => (
   <Container>
     <Text { ...props } />
     <Clock { ...props } />
   </Container>
-))
+)
