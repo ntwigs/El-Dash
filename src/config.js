047da46ebf5ced611a3b822cc33b898b33747e31
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-09-24 19:10:41 +0200

    replaced env with config

---
+++ src/config.js
@@ -0,0 +1,9 @@
+export const colors = {
+  background: '#070707',
+  foreground: '#253e4a',
+}
+
+export const application = {
+  githubApi: 'xxx-xxx-xxx-xxx',
+  githubUsername: 'Your-github-username',
+}
