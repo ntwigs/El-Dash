cb760bcd071476b420c548fc89cf47f208c6ec84
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-09-24 23:12:55 +0200

    Fix wrong commits bug

--- src/components/information/CommitAmount.js
+++ src/components/information/CommitAmount.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import styled from 'styled-components'
-import { compose, withState, lifecycle } from 'recompose'
+import { compose, withState, lifecycle, defaultProps } from 'recompose'
 import { Number } from '../common/PixelSymbol'
 import * as display from '../../utils/numbers'
 import { getCommits } from '../../http/commits'
@@ -17,24 +17,40 @@ const getValue = ({ index, commits }) => {
   return display[numberAsText]
 }
 
+const getBlanks = (commits, amountOfNumbers) => () => {
+  const commitLength = commits.toString().length
+  return amountOfNumbers - commitLength
+}
+
+const setState = async ({ props }) => {
+  const commits = await getCommits()
+  props.setBlanks(getBlanks(commits, props.amountOfNumbers))
+  props.setCommits(commits)
+}
+
 const enhance = compose(
   withState('commits', 'setCommits', 0),
+  withState('blanks', 'setBlanks', 0),
+  defaultProps({ amountOfNumbers: 5 }),
   lifecycle({
     async componentDidMount() {
-      this.props.setCommits(await getCommits())
-      setInterval(async () => {
-        this.props.setCommits(await getCommits())
-      }, 10000)
+      const fiveSecondDelay = 5000;
+      setState(this)
+      setInterval(async () => setState(this), fiveSecondDelay)
     },
   }),
 )
 
-const getNumbers = ({ commits }) =>
-  new Array(5)
+const getNumbers = ({ commits, blanks, amountOfNumbers }) =>
+  new Array(amountOfNumbers)
     .fill(<div />)
-    .map((tag, index) => <Number key={ index } display={ getValue({ index, commits }) } />)
-    .reverse()
+    .map((tag, index) => (
+      <Number
+        key={ index }
+        display={ getValue({ index: blanks - index, commits }) }
+      />
+    ))
 
-export const Clock = enhance(({ commits }) => (
-  <NumberContainer>{getNumbers({ commits })}</NumberContainer>
+export const Clock = enhance(({ commits, blanks, amountOfNumbers }) => (
+  <NumberContainer>{getNumbers({ commits, blanks, amountOfNumbers })}</NumberContainer>
 ))
