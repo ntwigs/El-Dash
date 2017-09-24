7a727763560d47e9c5a871a6ae1af678fa2d05d0
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-09-24 19:14:00 +0200

    Changed config to default

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-import dotenv from 'dotenv'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import { App } from './App'
 import registerServiceWorker from './registerServiceWorker'
-
-dotenv.config()
 
 window.React = React
 ReactDOM.render(<App />, document.getElementById('root'))
