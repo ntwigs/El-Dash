9198f60ed5913ad6f13787372e50d72886239ed1
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-09-30 21:40:00 +0200

    Changed to grid

--- src/components/common/PixelSymbol.js
+++ src/components/common/PixelSymbol.js
@@ -3,42 +3,33 @@ import styled from 'styled-components'
 import { colors } from '../../config'
 
 const sizeConfig = {
-  size: 30,
+  size: 10,
   width: 3,
-  margin: 2,
   height: 5,
-  mainWidth: 4,
-}
-
-const calculateSize = () => {
-  const { size, margin, mainWidth } = sizeConfig
-  return (size + margin) * mainWidth
 }
 
 const PixelFadeIn = styled.div`
-  backface-visibility: hidden;
-  width: ${ sizeConfig.size }px;
-  height: ${ sizeConfig.size }px;
+  width: ${ ({ small }) => (small ? sizeConfig.size / 5 : sizeConfig.size) }px;
+  height: ${ ({ small }) => (small ? sizeConfig.size / 5 : sizeConfig.size) }px;
   border-radius: 1px;
-  margin: ${ sizeConfig.margin }px;
   background-color: ${ ({ color }) => color };
 `
 
 const NumberContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  width: 100%;
-  max-width: ${ calculateSize() }px;
-  min-width: ${ calculateSize() }px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: ${ sizeConfig.size / 10  }px;
 `
 
-const generateNumber = ({ number }) =>
+const generateNumber = ({ display, small }) =>
   new Array(sizeConfig.height * sizeConfig.width)
     .fill(<div />)
     .map((tag, index) => (
-      <PixelFadeIn color={ number[index] ? colors.foreground : colors.background } key={ index } />
+      <PixelFadeIn
+        small={ small }
+        color={ display[index] ? colors.foreground : colors.background }
+        key={ index }
+      />
     ))
 
-export const Number = ({ display: number }) => (
-  <NumberContainer>{generateNumber({ number })}</NumberContainer>
-)
+export const Number = props => <NumberContainer>{generateNumber(props)}</NumberContainer>
