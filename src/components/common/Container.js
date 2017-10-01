7fa6a02977940c6b7f7b59c79a3a8ffc242c677b
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-10-01 14:08:27 +0200

    Replaced grid with flex

--- src/components/common/Container.js
+++ src/components/common/Container.js
@@ -6,11 +6,6 @@ const StyledContainer = styled.div`
   margin: ${ ({ small }) => (small ? 2 : 5) }px;
 `
 
-  // display: grid;
-  // grid-template-columns: repeat(${ ({ amount }) => amount }, 1fr);
-  // grid-gap: ${ ({ small }) => (small ? 3 : 10) }px;
-  // background-color: red;
-
 export const Container = ({ children, small, amount }) => (
   <StyledContainer small={ small } amount={ amount }>
     {children}
