8245345d4928e4fd4ab8c67a0d698089c91d5ce0
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-10-01 20:25:03 +0200

    Added remaining letters

--- src/util/letters.js
+++ src/util/letters.js
@@ -14,6 +14,14 @@ export const a = [
   true, false, true,
 ]
 
+export const b = [
+  true, true, false,
+  true, false, true,
+  true, true, false,
+  true, false, true,
+  true, true, false,
+]
+
 export const c = [
   true, true, true,
   true, false, false,
@@ -22,18 +30,42 @@ export const c = [
   true, true, true,
 ]
 
-export const o = [
-  true, true, true,
+export const d = [
+  true, true, false,
   true, false, true,
+  true, false, true,
+  true, false, true,
+  true, true, false,
+]
+
+export const e = [
+  true, true, true,
+  true, false, false,
+  true, true, false,
+  true, false, false,
+  true, true, true,
+]
+
+export const f = [
+  true, true, true,
+  true, false, false,
+  true, true, false,
+  true, false, false,
+  true, false, false,
+]
+
+export const g = [
+  true, true, true,
+  true, false, false,
   true, false, true,
   true, false, true,
   true, true, true,
 ]
 
-export const m = [
+export const h = [
+  true, false, true,
   true, false, true,
   true, true, true,
-  true, false, true,
   true, false, true,
   true, false, true,
 ]
@@ -46,12 +78,76 @@ export const i = [
   false, true, false,
 ]
 
-export const t = [
+export const j = [
   true, true, true,
-  false, true, false,
-  false, true, false,
-  false, true, false,
-  false, true, false,
+  false, false, true,
+  false, false, true,
+  true, false, true,
+  true, true, true,
+]
+
+export const k = [
+  true, false, true,
+  true, false, true,
+  true, true, false,
+  true, false, true,
+  true, false, true,
+]
+
+export const l = [
+  true, false, false,
+  true, false, false,
+  true, false, false,
+  true, false, false,
+  true, true, true,
+]
+
+export const m = [
+  true, false, true,
+  true, true, true,
+  true, false, true,
+  true, false, true,
+  true, false, true,
+]
+
+export const n = [
+  true, false, true,
+  true, false, true,
+  true, true, true,
+  true, true, true,
+  true, false, true,
+]
+
+export const o = [
+  true, true, true,
+  true, false, true,
+  true, false, true,
+  true, false, true,
+  true, true, true,
+]
+
+export const p = [
+  true, true, true,
+  true, false, true,
+  true, true, true,
+  true, false, false,
+  true, false, false,
+]
+
+export const q = [
+  true, true, true,
+  true, false, true,
+  true, false, true,
+  true, true, true,
+  true, true, true,
+]
+
+export const r = [
+  true, true, true,
+  true, false, true,
+  true, true, true,
+  true, true, false,
+  true, false, true,
 ]
 
 export const s = [
@@ -62,10 +158,58 @@ export const s = [
   true, true, true,
 ]
 
-export const e = [
+export const t = [
   true, true, true,
-  true, false, false,
-  true, true, false,
+  false, true, false,
+  false, true, false,
+  false, true, false,
+  false, true, false,
+]
+
+export const u = [
+  true, false, true,
+  true, false, true,
+  true, false, true,
+  true, false, true,
+  true, true, true,
+]
+
+export const v = [
+  true, false, true,
+  true, false, true,
+  true, false, true,
+  true, false, true,
+  false, true, false,
+]
+
+export const w = [
+  true, false, true,
+  true, false, true,
+  true, false, true,
+  true, true, true,
+  true, false, true,
+]
+
+export const x = [
+  true, false, true,
+  true, false, true,
+  false, true, false,
+  true, false, true,
+  true, false, true,
+]
+
+export const y = [
+  true, false, true,
+  true, false, true,
+  true, true, true,
+  false, true, false,
+  false, true, false,
+]
+
+export const z = [
+  true, true, true,
+  false, false, true,
+  false, true, false,
   true, false, false,
   true, true, true,
 ]
