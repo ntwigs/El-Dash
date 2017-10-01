4a52a042a9f2382958d36842e652955428e7c48c
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-10-01 23:50:35 +0200

    Using generated characters

--- src/components/information/Commits.js
+++ src/components/information/Commits.js
@@ -1,44 +1,25 @@
 import * as React from 'react'
-import styled from 'styled-components'
 import { compose, withState, lifecycle, defaultProps } from 'recompose'
-import { Number } from '../common/PixelSymbol'
-import * as display from '../../utils/numbers'
 import { getCommits } from '../../http/commits'
 import { Container } from '../common/Container'
-
-const getValue = ({ index, commits }) => {
-  const number = commits.toString()[index] || '0'
-  const numberAsText = display.getNumberAsText({ number })
-  return display[numberAsText]
-}
-
-const getBlanks = (commits, amount) => () => {
-  const commitLength = commits.toString().length
-  return amount - commitLength
-}
+import { generateCharacters } from '../common/generateCharacters'
 
 const setState = async ({ props }) => {
   const commits = await getCommits()
-  props.setBlanks(getBlanks(commits, props.amount))
   props.setCommits(commits)
 }
 
-const getNumbers = ({ commits, blanks, animation }, amount, small) => (
-  new Array(amount)
-    .fill(<div />)
-    .map((tag, index) => (
-      <Number
-        key={ index }
-        display={ getValue({ index: index - blanks, commits }) }
-        animation={ animation }
-        small={ small }
-      />
-    ))
-)
+const getNumbers = ({ commits, blanks, ...props }) => {
+  const value = commits.toString().split()
+  const asArr = new Array(props.amount)
+    .fill(0)
+    .reduce((acc, v, i) => (value[i] ? [...acc, value[i]] : [v, ...acc]), [])
+  const generatedCharacters = generateCharacters(asArr)
+  return generatedCharacters(props)
+}
 
 const enhance = compose(
   withState('commits', 'setCommits', 0),
-  withState('blanks', 'setBlanks', 0),
   defaultProps({ amount: 5 }),
   lifecycle({
     async componentDidMount() {
@@ -50,5 +31,5 @@ const enhance = compose(
 )
 
 export const Commits = enhance(({ amount, small, ...props }) => (
-  <Container { ...{ small, amount } }>{getNumbers(props, amount, small)}</Container>
+  <Container { ...{ small, amount } }>{getNumbers({ ...{ ...props, amount, small } })}</Container>
 ))
