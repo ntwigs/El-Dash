a29d4f784f7460472f7b710f16d34072b9655fb5
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-10-01 13:08:52 +0200

    Changed container from grid to flex

--- src/components/common/Container.js
+++ src/components/common/Container.js
@@ -2,10 +2,14 @@ import * as React from 'react'
 import styled from 'styled-components'
 
 const StyledContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(${ ({ amount }) => amount }, 1fr);
-  grid-gap: ${ ({ small }) => (small ? 3 : 10) }px;
+  display: flex;
+  margin: ${ ({ small }) => (small ? 2 : 5) }px;
 `
+
+  // display: grid;
+  // grid-template-columns: repeat(${ ({ amount }) => amount }, 1fr);
+  // grid-gap: ${ ({ small }) => (small ? 3 : 10) }px;
+  // background-color: red;
 
 export const Container = ({ children, small, amount }) => (
   <StyledContainer small={ small } amount={ amount }>
