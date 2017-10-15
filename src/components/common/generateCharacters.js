8630e85264a8fc79dd2121c75b245a74018a9b7f
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-10-15 23:53:23 +0200

    props refactor

--- src/components/common/generateCharacters.js
+++ src/components/common/generateCharacters.js
@@ -2,9 +2,9 @@ import * as React from 'react'
 import { Character } from './Character'
 import { symbols } from '../../util/symbols'
 
-export const generateCharacters = text => ({ amount, small, animation }) =>
+export const generateCharacters = text => ({ amount, ...props }) =>
   new Array(amount)
     .fill(<div />)
     .map((tag, index) => (
-      <Character key={ index } display={ symbols[text[index]] } { ...{ small, animation } } />
+      <Character key={ index } display={ symbols[text[index]] } { ...props } />
     ))
