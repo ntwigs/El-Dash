4eba805a54a986dd85b90b6607e66e153f29c1c2
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-10-16 00:36:48 +0200

    Use implemented routes

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,6 @@
 import * as Hapi from 'hapi'
+import { routes } from './routes'
+
 const server = new Hapi.Server()
 const PORT = 8081
 
@@ -6,5 +8,7 @@ server.connection({
   host: 'localhost',
   port: 8081,
 })
+
+server.route(routes)
 
 server.start(() => console.log(`Server up and running on port: ${PORT}`))
