1373dd1890c197fc64e5a41ad9a9e69e8a5517ad
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-10-18 23:12:17 +0200

    Maps weather data

--- src/components/information/Weather.js
+++ src/components/information/Weather.js
@@ -1,8 +1,8 @@
 import * as React from 'react'
-import { compose, withState, lifecycle, mapProps } from 'recompose'
+import { compose, withState, lifecycle, mapProps, defaultProps } from 'recompose'
 import { Container } from '../common/Container'
 import { generateCharacters } from '../common/generateCharacters'
-import { getWeather } from '../../http/weather'
+import weather from '../../static/weather.json'
 
 const getWeatherText = ({ temperature, ...props }) => {
   const generatedCharacters = generateCharacters(temperature)
@@ -11,15 +11,24 @@ const getWeatherText = ({ temperature, ...props }) => {
 
 const enhance = compose(
   withState('temperature', 'setTemperature', 0),
-  mapProps(props =>
-    Object.assign({}, props, {
-      temperature: props.temperature.toString().toLowerCase().split(''),
-      amount: props.temperature.length,
-    }),
-  ),
+  defaultProps({ temperature: 0, farenheit: false }),
+  mapProps(({ temperature, farenheit, ...props }) => {
+    const scale = farenheit ? 'f' : 'c'
+    const temperatureScale = farenheit ? temperature * (9 / 5) + 32 : temperature
+    const temperatureArray = Math.round(temperatureScale)
+      .toString().concat('*', scale)
+      .toLowerCase()
+      .split('')
+    return Object.assign({}, props, {
+      temperature: temperatureArray,
+      amount: temperatureArray.length,
+    })
+  }),
   lifecycle({
     async componentDidMount() {
-      this.props.setTemperature(await getWeather())
+      setInterval(() => {
+        this.props.setTemperature(weather.weather)
+      }, 5000)
     },
   }),
 )
