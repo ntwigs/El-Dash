b19ebfa9b6b7ee7a891f3a2f29d14925d8f7aa53
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-10-29 12:21:58 +0100

    Added cors to server

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,13 @@
 import Koa from 'koa'
 import compose from 'koa-compose'
+import cors from 'koa2-cors'
 import { weatherRouter } from './routes/weather'
 import { githubRouter } from './routes/github'
 
 const app = new Koa()
 const PORT = 3000
 
+app.use(cors())
 app.use(compose([weatherRouter.routes(), githubRouter.routes()]))
 
 app.listen(PORT, () => console.log(`SERVER: ${PORT}`))
