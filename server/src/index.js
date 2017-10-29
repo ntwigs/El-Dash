7b666532375ba6eaf6c894780cd9984634a515d2
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-10-29 23:34:25 +0100

    composes the youtube route

--- server/src/index.js
+++ server/src/index.js
@@ -3,11 +3,12 @@ import compose from 'koa-compose'
 import cors from 'koa2-cors'
 import { weatherRouter } from './routes/weather'
 import { githubRouter } from './routes/github'
+import { youtubeRouter } from './routes/youtube'
 
 const app = new Koa()
 const PORT = 3000
 
 app.use(cors())
-app.use(compose([weatherRouter.routes(), githubRouter.routes()]))
+app.use(compose([weatherRouter.routes(), githubRouter.routes(), youtubeRouter.routes()]))
 
 app.listen(PORT, () => console.log(`SERVER: ${PORT}`))
