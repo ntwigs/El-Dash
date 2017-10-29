995c97af8ed8da5eca2c4903d8973d23149901a9
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-10-29 12:22:11 +0100

    Weather uses proxy server

--- client/src/components/information/Weather.js
+++ client/src/components/information/Weather.js
@@ -1,22 +1,35 @@
 import * as React from 'react'
-import { compose, withState, lifecycle, mapProps, defaultProps } from 'recompose'
+import {
+  compose,
+  withState,
+  lifecycle,
+  mapProps,
+  defaultProps,
+} from 'recompose'
 import { Container } from '../common/Container'
 import { generateCharacters } from '../common/generateCharacters'
-import weather from '../../../data/weather.json'
 
 const getWeatherText = ({ temperature, ...props }) => {
   const generatedCharacters = generateCharacters(temperature)
   return generatedCharacters(props)
 }
 
+const setState = async ({ props }) => {
+  const res = await fetch('http://localhost:3000/weather')
+  const weather = await res.json()
+}
+
 const enhance = compose(
-  withState('temperature', 'setTemperature', 0),
+  withState('temperature', 'setTemperature', { celsius: 0, farenheit: 0 }),
   defaultProps({ temperature: 0, farenheit: false }),
   mapProps(({ temperature, farenheit, ...props }) => {
     const scale = farenheit ? 'f' : 'c'
-    const temperatureScale = farenheit ? temperature * (9 / 5) + 32 : temperature
-    const temperatureArray = Math.round(temperatureScale)
-      .toString().concat('*', scale)
+    const temperatureScale = farenheit
+      ? temperature.farenheit
+      : temperature.celsius
+    const temperatureArray = temperatureScale
+      .toString()
+      .concat('*', scale)
       .toLowerCase()
       .split('')
     return Object.assign({}, props, {
@@ -26,13 +39,15 @@ const enhance = compose(
   }),
   lifecycle({
     async componentDidMount() {
-      setInterval(() => {
-        this.props.setTemperature(weather.weather)
-      }, 5000)
+      const hour = 3600000
+      setState(this)
+      setInterval(async () => {
+        setState(this)
+      }, hour)
     },
   }),
 )
 
 export const Weather = enhance(props => (
-  <Container { ...props }>{getWeatherText({ ...props })}</Container>
+  <Container {...props}>{getWeatherText({ ...props })}</Container>
 ))
