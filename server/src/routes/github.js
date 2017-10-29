5273e463afb82978a5069f9e6e226375fbe9b74c
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-10-29 12:00:17 +0100

    Gets commits

---
+++ server/src/routes/github.js
@@ -0,0 +1,26 @@
+import Router from 'koa-router'
+import { github } from '../config'
+import { jFetch } from '../util/jFetch'
+
+const router = new Router()
+
+router.get('/commits', async (ctx, next) => {
+  const headers = {
+    headers: {
+      Authorization: `token ${ github.key }`,
+      'User-Agent': github.username,
+    },
+  }
+  const events = await jFetch(`https://api.github.com/users/${ github.username }/events`, headers)
+  const today = new Date().getDate()
+  const commits = events
+    .filter(
+      ({ created_at, payload }) => today === new Date(created_at).getDate() && 'commits' in payload,
+    )
+    .map(({ payload }) => payload.commits.length)
+    .reduce((acc, val) => acc + val, 0)
+
+  ctx.body = { commits }
+})
+
+export const githubRouter = router
