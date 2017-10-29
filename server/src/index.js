96a69e09ce8d91e5e27c31690ec140a48d20604a
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-10-29 12:00:34 +0100

    Uses routes with koa-compose

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,11 @@
 import Koa from 'koa'
+import compose from 'koa-compose'
+import { weatherRouter } from './routes/weather'
+import { githubRouter } from './routes/github'
+
 const app = new Koa()
 const PORT = 3000
 
-app.use(ctx => ctx.body = 'Hey')
+app.use(compose([weatherRouter.routes(), githubRouter.routes()]))
 
-app.listen(PORT, () => console.log(`SERVER: ${ PORT }`))
+app.listen(PORT, () => console.log(`SERVER: ${PORT}`))
