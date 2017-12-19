93dbce243aa3e32f612cf468edd660ac8b70ef00
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-12-19 18:57:31 +0100

    Displays error if no channel is found

--- server/src/routes/youtube.js
+++ server/src/routes/youtube.js
@@ -6,10 +6,22 @@ const router = new Router()
 router.get('/youtube/:channelName', async (ctx, next) => {
   const { channelName } = ctx.params
   const { items } = await jFetch(`
-    https://www.googleapis.com/youtube/v3/channels?part=statistics&key=${youtube.key}%20&forUsername=${channelName}
+    https://www.googleapis.com/youtube/v3/channels?part=statistics&key=${
+      youtube.key
+    }%20&forUsername=${channelName}
   `)
-  const { hiddenSubscriberCount, ...display } = items[0].statistics
-  ctx.body = display
+  if (items.length) {
+    const { hiddenSubscriberCount, ...display } = items[0].statistics
+    ctx.body = display
+  } else {
+    ctx.status = 404
+    ctx.body = {
+      viewCount: 'error',
+      commentCount: 'error',
+      subscriberCount: 'error',
+      videoCount: 'error',
+    }
+  }
 })
 
 export const youtubeRouter = router
