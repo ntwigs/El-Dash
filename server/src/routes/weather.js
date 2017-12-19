9ca5bef873aac6a3d6421cd9957c336f46abce42
Northerntwig <oscar.cv.nordquist@gmail.com>
2017-12-19 18:28:29 +0100

    Gets weather

--- server/src/routes/weather.js
+++ server/src/routes/weather.js
@@ -2,13 +2,14 @@ import Router from 'koa-router'
 import { forecast } from '../config'
 import { toCelsius } from '../util/toCelsius'
 import { jFetch } from '../util/jFetch'
+import { checkKey } from './middleware/checkKey'
 
 const router = new Router()
 
-router.get('/weather', async (ctx, next) => {
+router.get('/weather', , async (ctx, next) => {
   const { loc } = await jFetch('http://ipinfo.io/json')
   const { currently } = await jFetch(
-    `https://api.darksky.net/forecast/${ forecast.key }/${ loc }`,
+    `https://api.darksky.net/forecast/${forecast.key}/${loc}`,
   )
   const farenheit = Math.round(currently.temperature)
   const celsius = Math.round(toCelsius(farenheit))
