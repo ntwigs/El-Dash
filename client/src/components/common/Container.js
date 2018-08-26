42dc550eb756658b1a77739dcfa4c5e2cf812575
Oscar Nordquist <oscar.cv.nordquist@gmail.com>
2018-08-26 21:00:41 +0200

    feat: stores dropped position of component

--- client/src/components/common/Container.js
+++ client/src/components/common/Container.js
@@ -1,11 +1,29 @@
 import * as React from 'react'
+import { compose, mapProps, withState, withHandlers } from 'recompose'
 import styled from 'styled-components'
+import Draggable from 'react-draggable'
 
-const StyledContainer = styled.div`
-  display: flex;
-  margin: ${ ({ small }) => (small ? 2 : 5) }px;
+const StyledContainer = styled(Draggable)`
+  margin: ${({ small }) => (small ? 2 : 5)}px;
 `
 
-export const Container = ({ children, ...props }) => (
-  <StyledContainer { ...props }>{ children }</StyledContainer>
+const CharacterContainer = styled.div`
+  display: flex;
+  margin: ${({ small }) => (small ? 2 : 5)}px;
+`
+
+const PureContainer = ({ children, ...props }) => (
+  <StyledContainer {...props} position={null}>
+    <CharacterContainer>{children}</CharacterContainer>
+  </StyledContainer>
 )
+
+// TODO: STORE EACH COMPONENT INSIDE LOCALSTORAGE WITH A POSITION ON DROP
+export const Container = compose(
+  withState('meta', 'setMeta', attributes => attributes),
+  withHandlers({
+    onStop: data => (_, { x, y }) => {
+      localStorage.setItem('location', 'wat')
+    },
+  })
+)(PureContainer)
