4bb744f78259fbfda703a955850566c7c6e8e8cb
ntwigs <oscar.cv.nordquist@gmail.com>
2019-08-15 21:24:29 +0200

    feat: Updated text component

--- client/src/components/information/Text.js
+++ client/src/components/information/Text.js
@@ -1,27 +1,22 @@
 import * as React from 'react'
-import Draggable from 'react-draggable'
-import { compose, mapProps, defaultProps } from 'recompose'
-import { Container } from '../common/Container'
-import { generateCharacters } from '../common/generateCharacters'
+import {compose, mapProps, defaultProps} from 'recompose'
+import {Container} from '../common/Container'
+import {generateCharacters} from '../common/generateCharacters'
 
-const getCommitText = ({ text, ...props }) => {
+const getCommitText = ({text, ...props}) => {
   const generatedCharacters = generateCharacters(text)
   return generatedCharacters(props)
 }
 
 const enhance = compose(
-  defaultProps({ text: 'text' }),
-  mapProps(props => {
-    console.log('>> props', props)
-    return props
-  }),
+  defaultProps({text: 'text'}),
   mapProps(props => ({
     ...props,
     text: props.text.toLowerCase().split(''),
     amount: props.text.length,
-  }))
+  })),
 )
 
 export const Text = enhance(props => (
-  <Container props={props}>{getCommitText(props)}</Container>
+  <Container {...props}>{getCommitText(props)}</Container>
 ))
