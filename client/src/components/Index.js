fbbcee3ca6ff6199d7f4f34ef3757f9be0aa0a4c
ntwigs <oscar.cv.nordquist@gmail.com>
2019-08-15 21:23:58 +0200

    fix: Uses new components

--- client/src/components/Index.js
+++ client/src/components/Index.js
@@ -1,54 +1,22 @@
 import * as React from 'react'
 import styled from 'styled-components'
-import { v1 as uuid } from 'uuid'
-import { colors } from '../config'
-import { withProps } from 'recompose'
-import { components } from '../util/components'
-import { generateComponent } from './common/generateComponent'
+import {GenerateComponent} from './common/generateComponent'
+import {colors} from '../config'
+import {Editor} from './editor'
+import {ComponentProvider} from './context'
 
 const IndexStyled = styled.section`
   width: 100vw;
   height: 100vh;
   background-color: ${colors.background};
-  display: flex;
-  justify-content: center;
-  align-items: center;
   overflow: hidden;
-  flex-direction: column;
 `
 
-const IndexPure = ({ components }) => (
-  <IndexStyled>{generateComponent({ components })}</IndexStyled>
+export const Index = () => (
+  <ComponentProvider>
+    <IndexStyled>
+      <Editor />
+      <GenerateComponent />
+    </IndexStyled>
+  </ComponentProvider>
 )
-
-export const Index = withProps({
-  components: [
-    {
-      type: components.text,
-      value: 'DRICKA VIN',
-      small: true,
-      position: { x: 50, y: 50 },
-      id: uuid(),
-    },
-    {
-      type: components.commits,
-      position: { x: 50, y: 100 },
-      id: uuid(),
-    },
-    {
-      type: components.subscribers,
-      position: { x: 50, y: 150 },
-      id: uuid(),
-    },
-    {
-      type: components.time,
-      position: { x: 50, y: 200 },
-      id: uuid(),
-    },
-    {
-      type: components.weather,
-      position: { x: 50, y: 250 },
-      id: uuid(),
-    },
-  ],
-})(IndexPure)
