92e737626e7f1b7bed1e89ee176353d03b455a75
ntwigs <oscar.cv.nordquist@gmail.com>
2019-08-15 21:24:25 +0200

    feat: Updated commit component

--- client/src/components/information/Commits.js
+++ client/src/components/information/Commits.js
@@ -1,30 +1,30 @@
 import * as React from 'react'
-import { compose, withState, lifecycle, defaultProps } from 'recompose'
-import { Container } from '../common/Container'
-import { generateCharacters } from '../common/generateCharacters'
+import {compose, withState, lifecycle, defaultProps} from 'recompose'
+import {Container} from '../common/Container'
+import {generateCharacters} from '../common/generateCharacters'
 
-const setState = async ({ props: { setCommits } }) => {
+const setState = async ({props: {setCommits}}) => {
   try {
     const res = await fetch('http://localhost:3001/commits')
-    const { commits } = await res.json()
+    const {commits} = await res.json()
     setCommits(commits)
-  } catch(err) {
+  } catch (err) {
     setCommits('api')
   }
 }
 
-const getNumbers = ({ commits, blanks, amount, ...props }) => {
+const getNumbers = ({commits, blanks, amount, ...props}) => {
   const value = commits.toString().split('')
   const asArr = Array(amount)
     .fill(0)
     .reduce((acc, v, i) => (value[i] ? [...acc, value[i]] : [v, ...acc]), [])
   const generatedCharacters = generateCharacters(asArr)
-  return generatedCharacters({ ...props, amount })
+  return generatedCharacters({...props, amount})
 }
 
 const enhance = compose(
   withState('commits', 'setCommits', 0),
-  defaultProps({ amount: 5 }),
+  defaultProps({amount: 5}),
   lifecycle({
     async componentDidMount() {
       const fiveSecondDelay = 5000
@@ -35,5 +35,5 @@ const enhance = compose(
 )
 
 export const Commits = enhance(props => (
-  <Container { ...props }>{getNumbers({ ...props })}</Container>
+  <Container {...props}>{getNumbers({...props})}</Container>
 ))
