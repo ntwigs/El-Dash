5f21277b82782e61b0983d6718862eef6ce64bab
ntwigs <oscar.cv.nordquist@gmail.com>
2019-08-18 16:34:09 +0200

    feat: Restructuring context

--- client/src/components/context.js
+++ client/src/components/context.js
@@ -4,35 +4,6 @@ import {v1 as uuid} from 'uuid'
 import {components} from '../util/components'
 import store from 'store'
 
-// const defaultContext = [
-//   {
-//     type: components.text,
-//     value: 'Oscar',
-//     position: {x: 50, y: 50},
-//     id: uuid(),
-//   },
-//   {
-//     type: components.commits,
-//     position: {x: 50, y: 100},
-//     id: uuid(),
-//   },
-//   {
-//     type: components.subscribers,
-//     position: {x: 50, y: 150},
-//     id: uuid(),
-//   },
-//   {
-//     type: components.time,
-//     position: {x: 50, y: 200},
-//     id: uuid(),
-//   },
-//   {
-//     type: components.weather,
-//     position: {x: 50, y: 250},
-//     id: uuid(),
-//   },
-// ]
-
 const COMPONENT_STRING = 'components'
 
 const ComponentContext = React.createContext()
@@ -42,9 +13,9 @@ const PureComponentProvider = ({
   resetComponents,
   addTextComponent,
   addTimeComponent,
+  addTodayComponent,
   addWeatherComponent,
-  addTimeTextComponent,
-  addDateComponent,
+  addWeekdayComponent,
   centerAllComponents,
   updatePosition,
   children,
@@ -55,9 +26,9 @@ const PureComponentProvider = ({
       resetComponents,
       addTextComponent,
       addTimeComponent,
+      addTodayComponent,
       addWeatherComponent,
-      addDateComponent,
-      addTimeTextComponent,
+      addWeekdayComponent,
       updatePosition,
       centerAllComponents,
     }}>
@@ -65,92 +36,117 @@ const PureComponentProvider = ({
   </ComponentContext.Provider>
 )
 
+const addWeatherComponent = ({
+  components: oldComponents,
+  setComponents,
+}) => () => {
+  const updatedComponents = [
+    ...oldComponents,
+    {
+      type: components.weather,
+      position: {x: 50, y: 50},
+      id: uuid(),
+    },
+  ]
+  store.set(COMPONENT_STRING)
+  setComponents(updatedComponents)
+}
+
+const addWeekdayComponent = ({components: oldComponents, setComponents}) => () => {
+  const updatedComponents = [
+    ...oldComponents,
+    {
+      type: components.date,
+      position: {x: 50, y: 50},
+      id: uuid(),
+    },
+  ]
+  store.set(COMPONENT_STRING)
+  setComponents(updatedComponents)
+}
+
+const addTodayComponent = ({
+  components: oldComponents,
+  setComponents,
+}) => () => {
+  const updatedComponents = [
+    ...oldComponents,
+    {
+      type: components.today,
+      position: {x: 50, y: 50},
+      id: uuid(),
+    },
+  ]
+  store.set(COMPONENT_STRING)
+  setComponents(updatedComponents)
+}
+
+const addTimeComponent = ({components: oldComponents, setComponents}) => () => {
+  const updatedComponents = [
+    ...oldComponents,
+    {
+      type: components.time,
+      position: {x: 50, y: 50},
+      id: uuid(),
+    },
+  ]
+  store.set(COMPONENT_STRING)
+  setComponents(updatedComponents)
+}
+
+const addTextComponent = ({components: oldComponents, setComponents}) => () => {
+  const updatedComponents = [
+    ...oldComponents,
+    {
+      type: components.text,
+      value: 'weather',
+      position: {x: 50, y: 50},
+      id: uuid(),
+    },
+  ]
+  store.set(COMPONENT_STRING, updatedComponents)
+  setComponents(updatedComponents)
+}
+
+const centerAllComponents = ({components, setComponents}) => () => {
+  const updatedComponents = components.map(({position, ...rest}) => ({
+    position: {x: 0, y: 0},
+    ...rest,
+  }))
+  store.set(COMPONENT_STRING, updatedComponents)
+  setComponents(updatedComponents)
+}
+
+const updatePosition = ({components, setComponents}) => ({id, e}) => {
+  const {x, y} = e
+  const updatedComponents = components.map(
+    component =>
+      id === component.id ? {...component, position: {x, y}} : component,
+  )
+  store.set(COMPONENT_STRING, updatedComponents)
+  setComponents(updatedComponents)
+}
+
+const resetComponents = ({setComponents}) => () => {
+  store.remove(COMPONENT_STRING)
+  setComponents([])
+}
+
 export const ComponentProvider = compose(
-  withState(COMPONENT_STRING, 'setComponents', store.get(COMPONENT_STRING) || []),
+  withState(
+    COMPONENT_STRING,
+    'setComponents',
+    store.get(COMPONENT_STRING) || [],
+  ),
   withHandlers({
-    resetComponents: ({setComponents}) => () => {
-      store.remove(COMPONENT_STRING)
-      setComponents([])
-    },
-    addWeatherComponent: ({components: oldComponents, setComponents}) => () => {
-      const updatedComponents = [
-        ...oldComponents,
-        {
-          type: components.weather,
-          position: {x: 50, y: 50},
-          id: uuid()
-        }
-      ] 
-      store.set(COMPONENT_STRING)
-      setComponents(updatedComponents)
-    },
-    addDateComponent: ({components: oldComponents, setComponents}) => () => {
-      const updatedComponents = [
-        ...oldComponents,
-        {
-          type: components.date,
-          position: {x: 50, y: 50},
-          id: uuid()
-        }
-      ] 
-      console.log('here')
-      store.set(COMPONENT_STRING)
-      setComponents(updatedComponents)
-    },
-    addTimeComponent: ({components: oldComponents, setComponents}) => () => {
-      const updatedComponents = [
-        ...oldComponents,
-        {
-          type: components.time,
-          position: {x: 50, y: 50},
-          id: uuid()
-        }
-      ] 
-      store.set(COMPONENT_STRING)
-      setComponents(updatedComponents)
-    },
-    addTimeTextComponent: ({components: oldComponents, setComponents}) => () => {
-      const updatedComponents = [
-        ...oldComponents,
-        {
-          type: components.text,
-          value: 'time',
-          position: {x: 50, y: 50},
-          id: uuid(),
-        },
-      ]
-      store.set(COMPONENT_STRING, updatedComponents)
-      setComponents(updatedComponents)
-    },
-    addTextComponent: ({components: oldComponents, setComponents}) => () => {
-      const updatedComponents = [
-        ...oldComponents,
-        {
-          type: components.text,
-          value: 'weather',
-          position: {x: 50, y: 50},
-          id: uuid(),
-        },
-      ]
-      store.set(COMPONENT_STRING, updatedComponents)
-      setComponents(updatedComponents)
-    },
-    centerAllComponents: ({components, setComponents}) => () => {
-      const updatedComponents = components.map(({position, ...rest}) => ({
-        position: {x: 0, y: 0},
-        ...rest,
-      }))
-      store.set(COMPONENT_STRING, updatedComponents)
-      setComponents(updatedComponents)
-    },
-    updatePosition: ({components, setComponents}) => ({id, e}) => {
-      const {x, y} = e
-      const updatedComponents = components.map(component =>
-        id === component.id ? {...component, position: {x, y}} : component,
-      )
-      store.set(COMPONENT_STRING, updatedComponents)
-      setComponents(updatedComponents)
-    },
+    resetComponents,
+    addWeatherComponent,
+    addWeekdayComponent,
+    addTimeComponent,
+    addTodayComponent,
+    addTextComponent,
+    centerAllComponents,
+    updatePosition,
   }),
 )(PureComponentProvider)
 
